7b52b0545a7a8d5bed6598a593941cb794923062
JuFer007 <juniorfernandozumaetagolac@gmail.com>
2025-08-25 22:48:52 -0500

    creacion de interfaces graficas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -13,10 +13,8 @@ module SystemHotel {
     requires com.almasb.fxgl.all;
     requires java.sql;
 
-    // Paquete donde están tus controladores FXML
     opens Formularios to javafx.fxml;
 
-    // Paquetes exportados para toda la app
     exports Formularios;
     exports Clases;
     exports ConexionBD;
